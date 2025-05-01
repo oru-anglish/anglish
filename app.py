83c5e3d9643e01164ba16d3be11a85933a31b65a
oru-anglish <oru.anglish@gmail.com>
2025-05-01 11:41:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,3 +31,6 @@ def analyze():
             "dep": dep_kor.get(token.dep_, token.dep_)
         })
     return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
